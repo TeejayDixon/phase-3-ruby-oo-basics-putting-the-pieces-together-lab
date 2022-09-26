e5789b632fe28ec9f0ea048e7dca85f70a5fddf9
Teejay Dixon <tahjgdixon@gmail.com>
2022-09-25 19:31:00 -0700

    commit tree 0d2598b11d99a86968842d5cfee60ff1b4af78c0

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,47 @@
 # Make your shoe class here!
+
+class Shoe 
+  
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def initialize(brand='Adidas')
+    @brand = brand
+    @color = color
+    @size = size 
+    @material = material 
+    @condition = condition
+  end
+
+  def condition
+    @condition
+  end
+
+
+ def cobble
+  if condition != 'old'
+   condition = 'new'
+    puts "Your shoe is as good as new!"
+  else 
+      condition = 'old'
+    end
+
+  end
+
+
+
+ 
+
+ 
+
+ 
+  
+    
+  
+
+
+end
+
+shoe = Shoe.new
+
+
+
